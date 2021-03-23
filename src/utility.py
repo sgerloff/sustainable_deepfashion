5b78b4f20a5293819e4d4d14020f2e956af9ee7a
Sergio Vechi <vechism@gmail.com>
2021-03-23 17:51:53 +0100

    create function to draw bounding boxes

--- src/utility.py
+++ src/utility.py
@@ -22,3 +22,32 @@ def get_hashsum_of_file(path_to_file):
                 break
             sha256.update(data)
     return sha256.hexdigest()
+
+
+def draw_bbox(path2image, bounding_box, color=(255, 255, 0), thickness=3):
+    """    
+    A simple function that draws a bounding box from a dataframe using the cv2.rectangle() method.
+    ----------------------------------------------------------------------------------------------  
+    Atributes:
+    ---------
+      path2image : str
+        path to the image         
+      bounding_box : list
+        bounding box [x1, y1, x2, y2]
+      color : tuple
+        RGB code for the box line, default color is yellow (255, 255, 0) 
+      thickness : int
+        line thickness
+    
+    Methods:
+    -------
+       cv2.rectangle()
+        draw a rectangle with a chosen color line borders of a chosen thickness 
+    """                      
+    start_point = (bounding_box[0], bounding_box[1]) # top left corner
+    end_point = (bounding_box[2], bounding_box[3])   # botton right corner
+
+    image = cv2.rectangle(path2image, start_point, end_point, color, thickness) 
+    plt.imshow(image)
+
+    return
