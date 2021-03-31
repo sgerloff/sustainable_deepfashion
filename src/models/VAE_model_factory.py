388979f4c2f98c7839f9968b979b0f20b692ecdf
Gert-JanD <gert-jan.dobbelaere@hotmail.com>
2021-03-31 18:17:36 +0200

    lower beta

--- src/models/VAE_model_factory.py
+++ src/models/VAE_model_factory.py
@@ -91,7 +91,7 @@ class VAE(models.Model):
             )
             kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
             kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
-            total_loss = reconstruction_loss + 0.1 * kl_loss
+            total_loss = reconstruction_loss + 0.01 * kl_loss
         grads = tape.gradient(total_loss, self.trainable_weights)
         self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
         self.total_loss_tracker.update_state(total_loss)
