9f8522a5cb889a71dc1bd3ba55532f4c04e3e9ac
Gert-JanD <gert-jan.dobbelaere@hotmail.com>
2021-04-04 19:52:35 +0200

    added visualization script and gif

--- scripts/pictures_to_GIF.sh
+++ scripts/pictures_to_GIF.sh
@@ -3,6 +3,6 @@ if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <input directory> <output file name>"
 else
   ffmpeg -i $1/%03d.jpeg $2.mp4
-  ffmpeg -i $2.mp4 -vf "fps=8,scale=224:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 $2.gif
+  ffmpeg -i $2.mp4 -vf "fps=10,scale=500:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 $2.gif
   rm $2.mp4
 fi
--- src/epoch_visualizations.py
+++ src/epoch_visualizations.py
@@ -1,12 +1,15 @@
 from src.instruction_utility import *
 import numpy as np
-import matplotlib.pyplot as plt
 import os
 from tqdm import tqdm
 from PIL import Image
+import pandas as pd
+import plotly.express as px
+from sklearn.decomposition import PCA
+from src.data.flat_dataset_factory import FlatDatasetFactory
 
 def predict_epochs(instruction_parser):
-    path = os.path.join(get_project_dir(), 'docs', 'assets', 'VAE_visuals', 'input', '000039_6.jpg')
+    path = os.path.join(get_project_dir(), 'docs', 'assets', 'VAE_visuals', 'input', '000029_4.jpg')
     image = Image.open(path)
     image = image.resize((224, 224))
     data_render = np.asarray(image) / 255.
@@ -18,7 +21,7 @@ def predict_epochs(instruction_parser):
     for file in tqdm(weight_files_list):
         model = instruction_parser.get_model()
         model.compile(optimizer=instruction_parser.get_optimizer())
-        model.built=True
+        model.built = True
         model.load_weights(file)
         reconstruction = model.predict(data_render)
         reconstruction_dict[file] = reconstruction
@@ -34,6 +37,61 @@ def save_image_reconstructions(reconstruction_dict):
         image.save(f'{target_path}/{index:03d}.jpeg')
         index += 1
 
+def predict_embeddings(instruction_parser):
+    database = load_dataframe("data/processed/category_id_1_min_pair_count_10_deepfashion_train.joblib")
+    model_factory = instruction_parser.model_factory
+    factory = FlatDatasetFactory(database, preprocessor=model_factory.preprocessor())
+    data = factory.get_dataset(batch_size=385, shuffle=False)
+
+    label_batches = []
+    for x, y in data.take(1):
+        images_preprocessed = x
+        label_batches.append(y)
+
+    labels = np.concatenate(label_batches)
+
+
+    weight_files_list = sorted([os.path.join(get_visuals_dir(), file) for file in os.listdir(get_visuals_dir())])
+    embedding_dict = {}
+
+    for file in tqdm(weight_files_list):
+        model = instruction_parser.get_model()
+        model.compile(optimizer=instruction_parser.get_optimizer())
+        model.built = True
+        model.load_weights(file)
+        _, _, embeddings = model.encoder.predict(images_preprocessed)
+        embedding_dict[file] = embeddings
+
+    return embedding_dict, labels
+
+def save_PCA_images(embedding_dict, labels):
+    database = load_dataframe("data/processed/category_id_1_min_pair_count_10_deepfashion_train.joblib")
+    index = 1
+    for file in embedding_dict.keys():
+        pca = PCA(n_components=2)
+        embeddings = embedding_dict[file]
+        pca.fit(embeddings)
+        components = pca.transform(embeddings)
+        pca_df = pd.DataFrame(components, columns=['pc1', 'pc2'])
+        pca_df['pair_id'] = labels
+        pca_df['pair_id'] = pca_df['pair_id'].astype(int)
+        pca_df['pair_id'] = pca_df['pair_id'].astype(str)
+
+        fig = px.scatter(pca_df,
+                         x='pc1',
+                         y='pc2',
+                         color='pair_id',
+                         range_x=(-30, 30),
+                         range_y=(-30, 30),
+                         color_discrete_sequence=px.colors.qualitative.Dark24,
+                         width=600,
+                         height=500)
+
+        target_path = os.path.join(get_project_dir(), 'docs', 'assets', 'VAE_visuals', 'embeddings')
+        fig.write_image(f'{target_path}/{index:03d}.jpeg')
+        index += 1
+
+
 
 
 def get_project_dir():
@@ -47,5 +105,7 @@ def get_visuals_dir():
 
 if __name__ == '__main__':
     instruction_parser = InstructionParser('VAE_conv2d_input_224_embedding_512.json')
-    reconstruction_dict = predict_epochs(instruction_parser)
-    save_image_reconstructions(reconstruction_dict)
+    # reconstruction_dict = predict_epochs(instruction_parser)
+    # save_image_reconstructions(reconstruction_dict)
+    embedding_dict, labels = predict_embeddings(instruction_parser)
+    save_PCA_images(embedding_dict, labels)
